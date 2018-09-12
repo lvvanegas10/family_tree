31f9366718a245f9cef661a7cf20b9d9f5c3853c
Valeria <lv.vanegas10@uniandes.edu.co>
2018-09-12 12:34:32 -0500

    Back

--- back/server/config/config.js
+++ back/server/config/config.js
@@ -18,8 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/tree';
 } else {
-    //CREAR LA VARIABLE DE ENTONRNO EN HEROKU PARA NO SUBIR ESTO
-    urlDB ='mongodb://tree-user:proyectoWeb2018@ds255262.mlab.com:55262/tree'
-    // urlDB = process.env.MONGO_URI;
+    urlDB = process.env.MONGO_URL;
 }
 process.env.URLDB = urlDB;
