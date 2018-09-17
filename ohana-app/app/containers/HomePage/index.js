d0b40f620594f960b464478ba49b8dbc8ee3f5b9
Tefa <tefa@MacBook-Pro-de-Tefa.local>
2018-09-16 19:32:21 -0500

    fixes loading tree

--- ohana-app/app/containers/HomePage/index.js
+++ ohana-app/app/containers/HomePage/index.js
@@ -12,7 +12,6 @@ import mainImage from 'images/mainImage.jpg';
 /* eslint-disable react/prefer-stateless-function */
 class HomePage extends React.PureComponent {
   render() {
-    
     return (
       <div>
         <Image src={mainImage} fluid />
